daa589e46354289a4e51d7acd060934aebb6d7e3
SabrinaLara <sabrinaaalejandra@gmail.com>
2021-04-12 12:32:37 -0300

    desafío 6

---
+++ assets/javascript/obj.js
@@ -0,0 +1,20 @@
+/*import { Sara, Andres} from "./obj.js";*/
+
+let sara = new Alumno("Sara", 19, 1, 7);
+let andres = new Alumno("Andres", 23, 1, 7);
+
+sara.paso();
+andres.paso();
+sara.agregar();
+sara.notaMateria();
+andres.agregar();
+andres.notaMateria();
+
+console.log(sara.obtenerNombre + " obtuvo el curso: " + sara.obtenerCurso);
+console.log(andres.obtenerNombre + " obtuvo el curso: " + andres.obtenerCurso);
+
+sara.cambioCurso = "2";
+console.log(sara.obtenerNombre + " cambió al curso: " + sara.obtenerCurso);
+
+
+
